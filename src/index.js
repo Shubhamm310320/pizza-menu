8639f8c5e737764942ec02dd12d6ec9e9eb6892c
Shubham Malviya <shubhamm310320@gmail.com>
2025-04-17 10:30:06 +0530

    React Fragments group elements without having the need to create another element in our DOM tree. Sometimes we need to add a key to fragments for example in a list, so we need to write it using React.Fragment

--- src/index.js
+++ src/index.js
@@ -70,6 +70,7 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       <Pizzas />
     </main>
   );
@@ -77,11 +78,18 @@ function Menu() {
 
 function Pizzas() {
   return pizzaData.length > 0 ? (
-    <ul className="pizzas">
-      {pizzaData.map((pizza) => (
-        <Pizza pizza={pizza} key={pizza.name} />
-      ))}
-    </ul>
+    <>
+      <p>
+        Authentic Italian cuisine, 6 creative dishes to choose from, All from
+        our stone oven, all organic, all delicious.
+      </p>
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizza={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </>
   ) : (
     <p>We're still working on our menu. Please come back later!😉</p>
   );
